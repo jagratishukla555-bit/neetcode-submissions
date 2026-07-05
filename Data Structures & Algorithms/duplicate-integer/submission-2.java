9f3e6200c10a1224d4e080d28492abead52e925a
Jagrati Shukla <jagratishukla555@gmail.com>
2026-07-05 14:08:22 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,31 @@
+class Solution{
+
+   public boolean hasDuplicate(int[] nums)
+    {
+        int n= nums.length;
+        int max =nums[0];
+          for(int i=1; i<n; i++)
+            {
+                if(nums[i] > max){
+                max=nums[i];
+            }
+            }
+
+
+        int[] freq = new int[max+1];
+
+        for(int i=0; i<n; i++)
+        {
+            freq[nums[i]]++;
+        }
+
+        for(int i=0; i<max+1; i++)
+        {
+            if(freq[i] > 1)
+            return true;
+        
+        }
+        return false;
+    
+   } 
+}
